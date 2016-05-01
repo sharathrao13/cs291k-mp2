cc2a9a7598bef9aaae81050e53910afdfd92e837
Sharath Rao <sharathrao13@gmail.com>
2016-04-30 18:44:30 -0700

    Chahnge momentum

--- conv_net.py
+++ conv_net.py
@@ -65,7 +65,7 @@ model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
 # let's train the model using SGD + momentum (how original).
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.95, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
