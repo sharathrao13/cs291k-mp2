75780004922d25a62b549a4535e8404f7f05d4cf
Sharath Rao <sharathrao13@gmail.com>
2016-05-04 17:51:36 -0700

    After Hyper params

--- redo.py
+++ redo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from __future__ import print_function
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
@@ -6,20 +8,23 @@ from keras.optimizers import SGD
 from keras.utils import np_utils
 import sys
 import data_utils
+import time
 
 if __name__ == '__main__':
+
+    start_time = time.time()
+    file_location = sys.argv[1]+"/cifar-100-python"
+    print(file_location)
+
+    print("****Please note that Keras/Theano prints the Training Loss and Training Accuracy as loss and accuracy****")
     batch_size = 32
     nb_classes = 20
-    nb_epoch = 10
-    data_augmentation = False
+    nb_epoch = 10 #This will take 10 hours approximately
 
-    # input image dimensions
     img_rows, img_cols = 32, 32
-    # the CIFAR10 images are RGB
     img_channels = 3
 
-    # the data, shuffled and split between train and test sets
-    (X_train, y_train,X_test, y_test) = data_utils.load_CIFAR100("dataset/cifar-100-python/")#cifar10.load_data()
+    (X_train, y_train,X_test, y_test) = data_utils.load_CIFAR100(file_location)
 
     X_val = X_train[49000:50000,:,:,:]
     y_val = y_train[49000:50000]
@@ -29,31 +34,14 @@ if __name__ == '__main__':
     y_test = y_test[0:10000]
 
 
-    print('X_train shape:', X_train.shape)
-    print('y_train shape:', y_train.shape)
-    print('X_val shape:', X_val.shape)
-    print('y_val shape:', y_val.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
-
-    # convert class vectors to binary class matrices
     Y_train = np_utils.to_categorical(y_train, nb_classes)
     Y_val = np_utils.to_categorical(y_val, nb_classes)
     Y_test = np_utils.to_categorical(y_test, nb_classes)
-    print('X_train shape:', X_train.shape)
-    print('y_train shape:', y_train.shape)
 
     model = Sequential()
 
-    model.add(Convolution2D(32, 3, 3, border_mode='same',
+    model.add(Convolution2D(64, 3, 3, border_mode='same',
                             input_shape=(img_channels, img_rows, img_cols)))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(32, 3, 3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3))
     model.add(Activation('relu'))
@@ -61,13 +49,12 @@ if __name__ == '__main__':
     model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(512))
+    model.add(Dense(1024))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
-    # let's train the model using SGD + momentum (how original).
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.95, nesterov=True)
     model.compile(loss='categorical_crossentropy',
                   optimizer=sgd,
@@ -87,5 +74,8 @@ if __name__ == '__main__':
                   shuffle=True)
 
     loss, accuracy = model.evaluate(X_test, Y_test, show_accuracy=True)
-    print("loss ",loss)
-    print("Accuracy: ",accuracy)
+    print("********************************************************************")
+    print("Testing Loss ",loss)
+    print("Testing Accuracy: ",accuracy)
+    print("********************************************************************")
+    print("Took %s seconds" % (time.time() - start_time))
